2281cd65459fe3f497bc8c342375e64995aa72a5
dcwoody <Jacob.wood@gmail.com>
2025-07-22 22:48:07 +0000

    648pm

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,15 @@
 // src/types/user.ts
 
+interface JobPreferences {
+  preferred_roles: string;
+  preferred_location: string;
+  salary_range: string;
+}
+
 export interface UserProfile {
   id: string;
-  auth_user_id: string;
   email: string;
   first_name?: string;
   last_name?: string;
-  created_at: string;
-  preferences?: Record<string, any>; // or define a shape if known
+  preferences?: JobPreferences;
 }
