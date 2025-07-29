cfe3fad0affcd53f56e859b5455b102ea1f9fadf
dcwoody <Jacob.wood@gmail.com>
2025-07-29 03:26:03 +0000

    1126 update

--- src/lib/supabaseServer.ts
+++
@@ -1,7 +0,0 @@
-// lib/supabaseServer.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
-
-export const supabaseServer = createClient(supabaseUrl, supabaseServiceKey)
