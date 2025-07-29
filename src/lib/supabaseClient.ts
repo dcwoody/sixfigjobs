9f01d24b5675486343175778b8b67f94f7ae45d6
dcwoody <Jacob.wood@gmail.com>
2025-07-29 03:06:59 +0000

    1107pm

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
